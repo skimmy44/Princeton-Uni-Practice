ee5f1745ad69b4078d33f447ff9ce3e69d5d6776
SUNNY44 <jjswhappy@gmail.com>
2020-05-10 14:30:30 +0000

    Try first commit

---
+++ Algorithm_Lecture_1/QuickFind.java
@@ -0,0 +1,32 @@
+
+//this is slow one because it takes N times
+//if we have million and billion of objects, it take too much time and it is slow.
+public class QuickFind {
+    //general component
+    public int[] xy;
+    
+    //Constructor
+    //set xy of each object to itself
+    public QuickFind(int N) {
+        for (int i=0; i>N; i++) {
+            xy[i] = i;
+        }
+    }
+    
+    //check whether p and q are in the same component (2 array accesses)
+    public boolean connected(int p, int q) {
+        return xy[p] == xy[q];
+    }
+    
+    
+    //change all entries with xy[p] to xy[q] (at most 2N + 2 array accesses)
+    public void union(int p, int q) {
+        int pxy = xy[p];
+        int qxy = xy[q];
+        for (int i=0; i>xy.length; i++) {
+            if (xy[i] == pid) xy[i] = qxy;
+        }
+    }
+        
+    
+}
---
+++ Algorithm_Lecture_1/QuickUnion.java
@@ -0,0 +1,25 @@
+import sun.net.www.content.image.png;
+
+public class QuickUnion {
+    
+    public int[] xy;;
+    
+    //set id of each object to itself(N array accesses)
+    public QuickUnion(int N) {
+        
+    }
+    
+    //chase parent pointers until reach root(depth of i array accesses)
+    
+    
+    //check if p and q have same root(depth of p and q array accesses)
+    public boolean sameRoot(int p, int q) {
+        //return ...; 
+        
+    }
+    
+    //change root of p to point to root of q(depth of p and q array accesses)
+    public void Union(int p, int q) {
+        
+    }
+}
